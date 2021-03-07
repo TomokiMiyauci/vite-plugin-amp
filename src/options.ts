83683d46154d8dd351fa3a0af70c57383074b61e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-08 03:02:42 +0900

    :bug:  Remove optimize on transform

--- src/options.ts
+++ src/options.ts
@@ -2,12 +2,7 @@ import { IndexHtmlTransformHook } from 'vite'
 import { ViteSSGOptions } from 'vite-ssg'
 
 import replacer from './replacer'
-const transform = (ampOptimizer: {
-  transformHtml: (html: string) => string
-}): IndexHtmlTransformHook => async (html, { bundle, server }) => {
-  if (!server?.config.isProduction) {
-    html = await ampOptimizer.transformHtml(html)
-  }
+const transform: IndexHtmlTransformHook = async (html, { bundle }) => {
   if (!bundle) return html
 
   const replaces = Object.entries(bundle).map(replacer)
